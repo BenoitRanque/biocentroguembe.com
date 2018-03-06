17d0e54f1087d7a6eca71372c58119602edf359b
Bienvenido Benoit Ranque <desarrollo@guembe.ti>
2018-03-06 17:03:48 -0400

    prototyping QImg

---
+++ client/src/components/QImg.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+
+https://github.com/Akryum/vue-observe-visibility
+https://www.sitepoint.com/how-to-build-your-own-progressive-image-loader/
+https://css-tricks.com/the-blur-up-technique-for-loading-background-images/
+
+export default {
+  name: 'QImg',
+  data () {
+    return {
+      loaded: false
+    }
+  },
+  props: {
+    responsive: {
+      type: Boolean
+    },
+    placeholder: {
+      type: Boolean
+    },
+    src: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    loadPlaceHolder () {
+      // load image placeholder
+    },
+    loadImage () {
+      // load actual image
+
+    }
+  },
+  mounted () {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
